e2469d07b4770eb1b44a28b7865e6253e115acec
ahmzz <ahmedminhas247@outlook.com>
2023-08-14 19:27:28 +0500

    Dashboard layout made responsive using media query hook in mui

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,11 +1,12 @@
-import { useTheme,Box } from '@mui/material'
+import { useTheme,Box,useMediaQuery } from '@mui/material'
 import React from 'react'
 
 const Dashboard = () => {
     
   const {palette}=useTheme()
+  const isScreenLarge=useMediaQuery('(min-width:1200px)')
 
-  const gridTemplate=`
+  const gridTemplateLargeScreen=`
     "a b c"
     "a b c"
     "a b c"
@@ -18,13 +19,52 @@ const Dashboard = () => {
     "g h j"
   `
 
+  const gridTemplateSmallScreen=`
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+  
+  `
+
   return (
-    <Box width="100%" height="100%" display="grid" gap="1.5rem"
-      sx={{
-        gridTemplateColumns:"repeat(3,minmax(370px,1fr))",
-        gridTemplateRows:"repeat(10,minmax(60px,1fr))",
-        gridTemplateAreas:gridTemplate
-      }}
+    <Box width="100%" height="100%" display="grid" gap="1.1rem"
+      sx={
+        isScreenLarge?{
+          gridTemplateColumns:"repeat(3,minmax(350px,1fr))",
+          gridTemplateRows:"repeat(10,minmax(50px,1fr))",
+          gridTemplateAreas:gridTemplateLargeScreen
+        }:{
+          gridAutoColumns:"1fr",
+          gridAutoRows:"70px",
+          gridTemplateAreas:gridTemplateSmallScreen
+        }
+      }
     >
       <Box bgcolor="#fff" gridArea="a"></Box>
       <Box bgcolor="#fff" gridArea="b"></Box>
